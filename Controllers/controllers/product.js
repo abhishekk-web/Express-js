7a93d520a3a94f2ce05c0e49a9ca36309c2aab71
abhishekk-web <84089009+abhishekk-web@users.noreply.github.com>
2022-10-29 04:14:11 -0700

    controller of contact

---
+++ Controllers/controllers/product.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+const rootDir = require("../util/path");
+
+exports.getContact = (req, res, next)=>{
+
+    res.sendFile(path.join(rootDir, "views", "contact.html"));
+
+}
+
+exports.postContact = (req, res, next)=>{
+
+    res.send("<h1>Form Successfully filled</h1>")
+
+}
